6d952ed0cd6354c77c750f7eb1622fbe13a78374
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2026-03-20 20:01:01 -0500

    backup

--- init.lua
+++ init.lua
@@ -4,9 +4,8 @@ assert(minetest.get_modpath('ia_util'))
 assert(ia_util ~= nil)
 local modname                    = minetest.get_current_modname() or "ia_scribe"
 local storage                    = minetest.get_mod_storage()
-ia_scribe                        = {
-
-}
+ia_scribe                        = { }
+ia_scribe.mod                    = 'ia'
 local modpath, S                 = ia_util.loadmod(modname)
 local log                        = ia_util.get_logger(modname)
 local assert                     = ia_util.get_assert(modname)
